569ca7c2a8faea143ef777bbe09d1a1034f7ec54
AlenaPopova <fruna@yandex.ru>
2017-05-13 20:19:45 +0300

    Создание нескольких функций

--- sendbox/src/main/java/ru/stqa/pft/sendbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/pft/sendbox/MyFirstProgram.java
@@ -4,7 +4,20 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    double l = 5.5;
+    System.out.println("Площадь квадрата" + "=" + area(l));
+
+    double a = 4.1;
+    double b = 3.2;
+    System.out.println("Площадь прямоугольника" + "=" + area(a,b));
   }
 
+  public static double area(double l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a*b;
+  }
 
 }
