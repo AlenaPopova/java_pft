505aef7b90ed07140c2bbb1cb7c344b289b9e00a
AlenaPopova <fruna@yandex.ru>
2017-05-15 22:59:21 +0300

    Домашнее задание № 2

--- sendbox/src/main/java/ru/stqa/pft/sendbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/pft/sendbox/MyFirstProgram.java
@@ -3,25 +3,30 @@ package ru.stqa.pft.sendbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    //System.out.println("Hello World!");
 
-    Square s = new Square(7); //s - переменная, New Square - создание нового объекта, в скобках значение параметра
+   // Square s = new Square(7); //s - переменная, New Square - создание нового объекта, в скобках значение параметра
 
-    System.out.println("Площадь квадрата со стороной " + s.l + "=" + area(s));
+    //System.out.println("Площадь квадрата со стороной " + s.l + "=" + area(s));
 
 
-    Rectangle r = new Rectangle(4,6);
+    //Rectangle r = new Rectangle(4,6);
    // r.a = 3.2; //заполнение атрибутов значениями
    // r.b = 6;
-  System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
-  }
+ // System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 
-  public static double area(Square s) {
-    return s.l * s.l;
+    ////////////////////////////////Домашнее задание №2
+    Point p1 = new Point(2,5);
+    Point p2 = new Point(6,6);
+    System.out.println(p1.distance(p2));
   }
+///////////////////////////////////////////////////
+ // public static double area(Square s) {
+  //  return s.l * s.l;
+  //}
 
-  public static double area(Rectangle r) {
-  return r.a*r.b;
-  }
+ // public static double area(Rectangle r) {
+  //return r.a*r.b;
+ // }
 
 }
--- sendbox/src/main/java/ru/stqa/pft/sendbox/Point.java
+++ sendbox/src/main/java/ru/stqa/pft/sendbox/Point.java
@@ -1,16 +1,19 @@
 package ru.stqa.pft.sendbox;
 
-import com.sun.org.apache.bcel.internal.generic.NEW;
-
 /**
- * Created by Алёна on 13.05.2017.
+ * Created by Алёна on 15.05.2017.
  */
 public class Point {
+double x;
+double y;
 
-  public static double P1(double x1, double y2) {
-  }
+public Point (double x, double y){ //создаем конструктор, имя конструктора = имени класса
+  this.x = x;
+  this.y = y;
+
 }
 
-
-  
-
+  public double distance( Point p2){
+  return Math.sqrt((Math.pow(p2.x-this.x,2))+(Math.pow(p2.y-this.y,2)));
+  }
+}
