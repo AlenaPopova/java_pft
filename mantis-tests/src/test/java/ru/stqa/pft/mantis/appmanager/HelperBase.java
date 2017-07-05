31e324d36760f6a7d1563e5e2f3fe1f96fbf21cf
AlenaPopova <fruna@yandex.ru>
2017-07-05 15:18:30 +0300

    Получение почты на почтовый сервер

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HelperBase.java
@@ -0,0 +1,54 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Алёна on 23.05.2017.
+ */
+public class HelperBase {
+  protected WebDriver wd;
+  protected ApplicationManager app;
+
+  public HelperBase(ApplicationManager app) {
+    this.app = app;
+    this.wd = app.getDriver();
+  }
+
+  protected void click(By locator) {
+    wd.findElement(locator).click();
+  }
+
+  protected boolean isElementPresent(By locator) {
+    try {
+      wd.findElement(locator);
+      return true;
+    } catch (NoSuchElementException ex){
+      return false;
+    }
+  }
+
+  protected void type(By locator, String text) {
+  click(locator);
+    if (text != null) {
+      String existingText = wd.findElement(locator).getAttribute("value");
+      if (! text.equals(existingText)){
+        //wd.findElement(locator).click();
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
+    }
+  }
+
+  public boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/model/MailMessage.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.mantis.model;
+
+/**
+ * Created by popovaa on 05.07.2017.
+ */
+public class MailMessage {
+
+    public String to;
+    public String text;
+
+    public MailMessage(String to, String next){
+        this.to = to;
+        this.text = text;
+    }
+}
