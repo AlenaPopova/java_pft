6350c4d22cf8bc948b5390814aa49183170baf6a
AlenaPopova <fruna@yandex.ru>
2017-07-05 12:02:23 +0300

    Передача файлов по протоколу FTP

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -18,59 +18,66 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-  private final Properties properties;
-  private WebDriver wd;
+    private final Properties properties;
+    private WebDriver wd;
 
-  private String browser;
-  private RegistrationHelper registrationHelper;
+    private String browser;
+    private RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
 
-  public ApplicationManager(String browser) {
-    this.browser = browser;
-    properties = new Properties();
-  }
-
-  public void init() throws IOException {
-    String target = System.getProperty("target", "local");
-    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-  }
-
-
-  public void stop() {
-    if (wd != null) {
-      wd.quit();
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
     }
-  }
 
-  public HttpSession newSession() {
-    return new HttpSession(this);
-  }
-
-  public String getProperty(String key) {
-    return properties.getProperty(key);
-  }
-
-  public RegistrationHelper registration() {
-    if (registrationHelper == null) {
-      registrationHelper = new RegistrationHelper(this);
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
     }
-    return registrationHelper;
-  }
 
 
-
-  public WebDriver getDriver() { // ленивая инициализация
-    if (wd==null){ // если драйвер не проинициализирован, то начинается цикл
-      if (Objects.equals(browser, BrowserType.FIREFOX)) {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-      } else if (Objects.equals(browser, BrowserType.CHROME)) {
-        wd = new ChromeDriver();
-      } else if (Objects.equals(browser, BrowserType.IE)) {
-        wd = new InternetExplorerDriver();
-      }
-      wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS); // если уже проинициализирован драйвер
-      wd.get(properties.getProperty("web.baseUrl"));
+    public void stop() {
+        if (wd != null) {
+            wd.quit();
+        }
     }
-    return wd;
-  }
+
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public FtpHelper ftp() {
+        if (ftp == null) {
+            ftp = new FtpHelper(this);
+        }
+        return ftp;
+    }
+
+
+    public WebDriver getDriver() { // ленивая инициализация
+        if (wd == null) { // если драйвер не проинициализирован, то начинается цикл
+            if (Objects.equals(browser, BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+            } else if (Objects.equals(browser, BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (Objects.equals(browser, BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS); // если уже проинициализирован драйвер
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
+    }
 }
 
